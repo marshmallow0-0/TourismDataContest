95d16322f8babede83e61be3fb85638067cb688d
marshmallow0-0 <possibleway23@gmail.com>
2024-05-06 02:43:34 +0900

    design: 사이드바 디자인 수정

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import styles from "./Sidebar.css"; // CSS 모듈로 불러오기
 //출처 https://ji-u.tistory.com/22
-const Sidebar = ({ width = 280, children }) => {
+const Sidebar = ({ width = 320, children }) => {
     const [isOpen, setOpen] = useState(false);
     const [xPosition, setX] = useState(width);
     const side = useRef();
@@ -25,9 +25,9 @@ const Sidebar = ({ width = 280, children }) => {
                         </svg>
                     </button>
                 )}
-                <div className="content p-40 relative w-1/6">
+                <div className="content p-20 relative w-1/6">
                     {!isOpen && (
-                        <button onClick={() => toggleMenu()} className="button relative left-20 top-10 w-40 h-40 z-50 transition duration-800 ease border-2 border-gray-600 rounded-full overflow-hidden">
+                        <button onClick={() => toggleMenu()} className="button relative left-20 top-5 w-24 h-24 z-50 transition duration-800 ease border-2 border-gray-600 rounded-full overflow-hidden">
                             <img src="./img/rightArrow2.png" alt="contact open button" className="openBtn ml-3 w-4 h-5" />
                         </button>
                     )}
