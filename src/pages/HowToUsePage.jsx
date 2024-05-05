cf7eb5ebaec7b743a2b0fc2a55f85400f76bda82
marshmallow0-0 <possibleway23@gmail.com>
2024-05-06 02:18:27 +0900

    feat: 관련 객체 클릭시 설명부분이 모달창으로 활성화

--- src/pages/HowToUsePage.jsx
+++ src/pages/HowToUsePage.jsx
@@ -12,7 +12,7 @@ import initializeDragAndDrop from '../functions/initializeDragAndDrop'
 import { useNavigate } from 'react-router-dom';
 import { Banners, NOTUPLOAD } from "../pages/data";
 import CheckBoxGroup from '../components/MainCheckBoxGroup';
-//import './modal.css';
+import './modal.css';
 
 const HowToUsePage = () => {
     const { ref, animation } = useObserver();
@@ -262,7 +262,7 @@ const HowToUsePage = () => {
                 variants={opacityVariants}
             >
 
-                <div className="flex justify-center items-center">
+                {/* <div className="flex justify-center items-center">
                     {howmodalIsOpen && (
                         <div className="modal">
                             <div className="modal-content">
@@ -277,7 +277,7 @@ const HowToUsePage = () => {
                             <div className="modal-content">
                                 <span className="close" onClick={aitoggleModal}>&times;</span>
                                 <p>ai search</p>
-                                <img src="../img/how_to_use.png" alt="howToUse"></img>
+                                <img src="../img/Ai_search.png" alt="aisearch"></img>
                             </div>
                         </div>
                     )}
@@ -286,7 +286,7 @@ const HowToUsePage = () => {
                             <div className="modal-content">
                                 <span className="close" onClick={droptoggleModal}>&times;</span>
                                 <p>drop</p>
-                                <img src="../img/Ai_search.png" alt="howToUseAiSearch"></img>
+                                <img src="../img/drop_box.png" alt="dropbox"></img>
                             </div>
                         </div>
                     )}
@@ -308,6 +308,170 @@ const HowToUsePage = () => {
                             </div>
                         </div>
                     )}
+                </div> */}
+
+                <div>
+                    {howmodalIsOpen && (
+                        <div className="modal">
+                            <div className="modal-content w-1/2">
+                                <span className="close" onClick={howtoggleModal}>&times;</span>
+                                <div className="flex flex-row justify-items-center justify-center">
+                                    <img className="" src="./img/how_to_use.png"></img>
+
+                                    <div className="ml-5 mt-10 mb-7 mr-5 font-Pretendard border-2 rounded-3xl border-indigo-700 w-1/3 text-indigo-900">
+                                        <h1 className="mt-5 text-xl text-center font-bold">How to use button</h1>
+                                        <br />
+                                        <div className="flex justify-center items-center">
+                                            <img className="w-1/3" src="./img/how_to_use_info.png" />
+                                        </div>
+                                        <br />
+                                        <div className="ml-3 mr-3">
+                                            <span className="mt-2 text-lg font-semibold">[How to use]버튼은 RIA프로그램의 설명창으로 이동합니다.</span>
+
+                                            <div className="mt-4 ml-2 text-sm">
+                                                <p> - 사용법은 이 페이지에 나와있습니다. </p>
+                                                <p className="mt-2"> - 더 궁금한 내용이나 이해가 되지 않는다면 Ria2024@gmail.com으로 메일을 보내주세요.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    )}
+                    {aimodalIsOpen && (
+                        <div className="modal">
+                            <div className="modal-content">
+                                <span className="close" onClick={aitoggleModal}>&times;</span>
+                                <div className="flex flex-row justify-items-center justify-center">
+                                    <img className="w-3/5" src="./img/Ai_search.png"></img>
+
+                                    <div className="ml-5 mt-10 mb-7 mr-5 font-Pretendard border-2 rounded-3xl border-indigo-700 w-2/5 text-indigo-900">
+                                        <h1 className="mt-5 text-xl text-center font-bold">Ai search button</h1>
+                                        <br />
+                                        <div className="flex justify-center items-center">
+                                            <img className="w-1/7" src="./img/Ai_search_info.png" />
+                                        </div>
+                                        <br />
+                                        <div className="ml-3 mr-3">
+                                            <span className="mt-2 text-base font-semibold">[Ai search]버튼은 최종적인 작업입니다.</span>
+
+                                            <div className="mt-4 ml-2 text-sm">
+                                                <p> - Drop Box에 이미지를 넣었다면 버튼이 파란색으로 활성화 됩니다. </p>
+                                                <p className="mt-2"> - 작동하기 위해선 아래의 조건을 모두 충족하여야 합니다.</p>
+                                            </div>
+                                            <p className="mt-4 text-base font-semibold"> &lt;Condition&gt; </p>
+                                            <div className="mt-4 ml-2 text-sm">
+                                                <p> 1. Drop box에 이미지를 올립니다. </p>
+                                                <p className="mt-2"> 2. Check box들을 기호에 맞게 설정합니다.</p>
+                                                <p className="mt-2"> 3. 이미지 자르기를 통해 가고싶은 장소를 구체화 합니다.</p>
+                                                <p className="ml-2 mt-2"> - 구체화 하고싶지 않다면 사용하지 않아도 됩니다.</p>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                    {dropmodalIsOpen && (
+                        <div className="modal">
+                            <div className="modal-content w-1/2">
+                                <span className="close" onClick={droptoggleModal}>&times;</span>
+
+                                <div className="flex flex-row justify-items-center justify-center">
+                                    <img className="w-3/5" src="./img/drop_box.jpg"></img>
+
+                                    <div className="ml-5 mt-10 mb-7 mr-5 font-Pretendard border-2 rounded-3xl border-indigo-700 w-2/5 text-indigo-900">
+                                        <h1 className="mt-5 text-xl text-center font-bold">Drop Box Area</h1>
+                                        <br />
+                                        <div className="flex justify-center items-center">
+                                            <img className="w-1/3 rounded-md border-dashed border border-gray-500" src="./img/in_drop_box.png" />
+                                        </div>
+                                        <br />
+                                        <div className="ml-3 mr-3">
+                                            <span className="mt-2 text-base font-semibold">[Drop box Area]영역은 이미지를 검색하기위한 기반 영역입니다.</span>
+
+                                            <div className="mt-4 ml-2 text-sm">
+                                                <p> - 이미지를 Drag & Drop으로 영역에 업로드 할 수 있습니다. </p>
+                                                <p className="mt-2"> - Drop box를 클릭하면 파일 업로드 창이 생성됩니다.</p>
+                                                <p className="mt-2"> - 자른 이미지를 올리고 싶을 경우 결과물을 Drag & Drop하여 올리실 수 있습니다.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
+                    {checkmodalIsOpen && (
+                        <div className="modal">
+                            <div className="modal-content w-3/5">
+                                <span className="close" onClick={checktoggleModal}>&times;</span>
+                                <div className="flex flex-row justify-items-center justify-center">
+                                    <img className="w-3/5" src="./img/check_box.png"></img>
+
+                                    <div className="ml-5 mt-10 mb-7 mr-5 font-Pretendard border-2 rounded-3xl border-indigo-700 w-2/5 text-indigo-900">
+                                        <h1 className="mt-5 text-xl text-center font-bold">Check box Area</h1>
+                                        <br />
+                                        <div className="flex justify-center items-center">
+                                            <img className="w-2/3" src="./img/check_box_info.png" />
+                                        </div>
+                                        <br />
+                                        <div className="ml-3 mr-3">
+                                            <span className="mt-2 text-base font-semibold">[Check box Area]영역은 결과물을 구체화 하는 작업입니다.</span>
+
+                                            <div>
+                                                <p className="mt-4 text-base font-semibold"> &lt;Essential&gt; </p>
+                                                <div className="ml-2 text-sm">
+                                                    <p> - Region의 한 영역을 선택하여야 합니다. </p>
+                                                    <p> - Facility의 한 영역을 선택하여야 합니다.</p>
+
+                                                    <div className="mt-5 grid grid-cols-2 font-sm">
+                                                        <p>역사(history)</p>
+                                                        <p>건물(building)</p>
+                                                        <p>휴양지(recration)</p>
+                                                        <p>자연(nature)</p>
+                                                        <p>문화(culture)</p>
+
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+                    )}
+                    {cropmodalIsOpen && (
+                        <div className="modal">
+                            <div className="modal-content">
+                                <span className="close" onClick={croptoggleModal}>&times;</span>
+
+                                <div className="flex flex-row justify-items-center justify-center">
+                                    <img src="./img/crop_image.png"></img>
+
+                                    <div className="ml-5 mt-10 mb-7 font-Pretendard border-2 rounded-3xl border-indigo-700 w-1/4 text-indigo-900">
+                                        <h1 className="mt-5 text-xl text-center font-bold">Image Crop Area</h1>
+                                        <br />
+                                        <div className="flex justify-center items-center">
+                                            <img className="w-1/3" src="./img/crop_info.png" />
+                                        </div>
+                                        <br />
+                                        <div className="ml-3 mr-3">
+                                            <span className="mt-2 text-base font-semibold">[Image Crop Area]영역은 이미지 자르기 도구를 제공하여 줍니다.</span>
+
+                                            <div className="mt-4 ml-2 text-sm">
+                                                <p> - 이미지를 잘라 내가 원하는 장소의 특징을 뽑을 수 있습니다. </p>
+                                                <p className="ml-5"> a.이 절차가 결과물을 더 상세하게 정의할 수 있습니다.</p>
+                                                <p className="mt-2"> - 이미지 자르기를 안할 경우 다른 요소들로 인해 원하는 결과가 나오지 않을 수 있습니다.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    )}
                 </div>
 
 
@@ -323,7 +487,7 @@ const HowToUsePage = () => {
                                 <p className="ml-1 text-lg">
                                     Ria Web Service를 사용하는 방법을 가르쳐 드리겠습니다!
                                     <br /><br />
-                                    밑의 화면에 버튼들이나 노란 글씨를 클릭해보세요!
+                                    밑줄이 있는 영역을 클릭해보세요!
                                 </p>
                             </div>
                         </article>
@@ -337,36 +501,37 @@ const HowToUsePage = () => {
                                 <div id="drag-drop-area" className="text-center flex flex-row justify-center ml-24">
                                     <label htmlFor="file-upload" className="mt-10 cursor-pointer" onClick={() => handleMouseOver('drop')}>
                                         {/* <input id="file-upload" type="file" className="hidden" onChange={handleFileChange} /> */}
-                                        {uploadedImage ? <img className="uploadImg max-w-lg h-60 flex rounded-md border-dashed border-2 border-gray-500 p-2" src={uploadedImage.src} alt="upload" /> : <img className="uploadImg w-72 h-60 flex rounded-md border-dashed border-2 border-gray-500 p-2" src={NOTUPLOAD[0].image} alt="noUpload" />}
+                                        {uploadedImage ? <img className="uploadImg max-w-lg h-60 flex rounded-md border-dashed border-2 border-gray-500 p-2" src={uploadedImage.src} alt="upload" /> : <img className="uploadImg w-72 h-60 flex rounded-md border-dashed border-2 border-gray-500 p-2" src={NOTUPLOAD[1].image} alt="noUpload" />}
                                         {/* <CheckBoxGroup onChange={handleCheckboxGroupChange} /> */}
                                     </label>
 
                                     <div id="uploaded-image-container" className="min-w-72 min-h-72 flex flex-col p-2 ml-10 justify-center items-left" style={backgroundImageStyle}  >
-                                        <div className="text-left mb-3 text-4xl font-Pretendard text-yellow-400" onClick={() => handleMouseOver('check')}>
-                                            Check Box  <br /> Area
+                                        <div className="text-left mb-3 text-4xl font-Pretendard underline underline-offset-4" onClick={() => handleMouseOver('check')}>
+                                            Check Box  <br /> Tip Area
                                         </div>
 
                                         <CheckBoxGroup onChange={handleCheckboxGroupChange} />
 
                                         {buttonVisible &&
                                             <button
-                                                className={`${isImageUploaded ? "bg-blue-700" : "bg-gray-600"} w-28 h-10 text-center mt-2 mb-2 rounded-md inline text-xs text-white font-Pretendard`} onClick={() => handleMouseOver('ai')}>
+                                                className={`${isImageUploaded ? "bg-indigo-700" : "bg-gray-600"} w-28 h-10 text-center mt-2 mb-2 rounded-md inline text-xs text-white font-Pretendard underline underline-offset-4`} onClick={() => handleMouseOver('ai')}>
                                                 Ai Search
                                             </button>
                                         }
-                                        <button id='how' className="w-28 h-10 text-center bg-lime-700 inline-block rounded-md px-2 py-2 text-xs text-white font-Pretendard " onClick={() => handleMouseOver('how')}>How to use</button>
+                                        <button id='how' className="w-28 h-10 text-center bg-indigo-700 inline-block rounded-md px-2 py-2 text-xs text-white font-Pretendard underline underline-offset-4" onClick={() => handleMouseOver('how')}>How to use</button>
                                     </div>
                                 </div >
 
-                                <div className='flex flex-row justify-center items-center mt-10 gap-20' >
+                                <div className='flex flex-row justify-center items-center gap-10 ml-12 mt-10' >
 
-                                    <div className="text-right">
+                                    <div className="text-right border-dashed border-2 border-gray-500 rounded-md">
                                         <div className="flex flex-col items-center">
-                                            <div className="ml-28">
-                                                {cropData && <img className="w-72 h-60" src={cropData} alt=" " />}
+                                            <div className="border">
+                                                {cropData ? (cropData && <img className=" mr-2 ml-2 mt-2 mb-2 w-72 h-60" src={cropData} alt=" " />) : (<img className="mr-2 ml-2 mt-2 mb-2 w-72 h-60" src="./img/no_image.png" />)}
                                             </div>
-                                            <p className="text-md font-Pretendard text-semibold text-yellow-400" onClick={() => handleMouseOver('crop')}>Cropped Image</p>
-
+                                            <br />
+                                            <p className="text-md font-Pretendard text-semibold underline underline-offset-4" onClick={() => handleMouseOver('crop')}>Cropped Image</p>
+                                            <br />
                                         </div>
                                     </div>
                                     <div className="flex flex-col ml-10">
@@ -387,7 +552,7 @@ const HowToUsePage = () => {
                                             checkOrientation={false}
                                             guides={true}
                                         />
-                                        <button className="mt-3 w-28 h-10 bg-blue-700 rounded-md px-2 py-2 text-xs text-white font-Pretendard" onClick={getCropData}>
+                                        <button className="mt-3 w-28 h-10 bg-indigo-700 rounded-md px-2 py-2 text-xs text-white font-Pretendard" onClick={getCropData}>
                                             Crop
                                         </button>
                                     </div>
