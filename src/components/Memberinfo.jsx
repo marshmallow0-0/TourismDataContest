04132cb3cf2be15199fe250c3d05d4e3558a5522
marshmallow0-0 <possibleway23@gmail.com>
2024-05-06 02:07:06 +0900

    design: 멤버 소개 배경색 변경

--- src/components/Memberinfo.jsx
+++ src/components/Memberinfo.jsx
@@ -11,26 +11,26 @@ export default function Memberinfo({ title, image, description }) {
             </article>
         </section>
         */
-       //grid grid-flow-row-dense
-            <div className="flex flex-row items-center justify-center w-auto">
-                <section style={{ whiteSpace: 'pre-line' }} className="basis-3/5 mt-10 bg-slate-100 rounded-xl w-auto md:p-0 dark:bg-slate-800 ">
-                    <img className="w-32 h-32 rounded-full mx-auto m-8" src='https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/thumnail.png' alt={title} width="120" height="120"/>
-                    <div className="pt-6 md:p-4 text-center md:text-left ">
-                        <blockquote>
-                            <p className="text-lg font-medium text-white">
-                                Ria Project Member
-                            </p>
-                        </blockquote>
-                        <figcaption className="font-medium">
-                            <div className="text-sky-500 dark:text-sky-400">
-                                {title}
-                            </div>
-                            <div className="text-slate-700 dark:text-slate-500">
-                                {description}
-                            </div>
-                        </figcaption>
-                    </div>
-                </section>
-            </div>
+        //grid grid-flow-row-dense
+        <div className="flex flex-row items-center justify-center w-auto">
+            <section style={{ whiteSpace: 'pre-line' }} className="basis-3/5 mt-10 rounded-xl w-auto md:p-0 bg-slate-800 ">
+                <img className="w-32 h-32 rounded-full mx-auto m-8" src='https://t1.daumcdn.net/localimg/localimages/07/mapapidoc/thumnail.png' alt={title} width="120" height="120" />
+                <div className="pt-6 md:p-4 text-center md:text-left ">
+                    <blockquote>
+                        <p className="text-lg font-medium text-white">
+                            Ria Project Member
+                        </p>
+                    </blockquote>
+                    <figcaption className="font-medium">
+                        <div className="text-sky-500 dark:text-sky-400">
+                            {title}
+                        </div>
+                        <div className="text-slate-700 dark:text-slate-500">
+                            {description}
+                        </div>
+                    </figcaption>
+                </div>
+            </section>
+        </div>
     );
 }
