59bf0c36f3565631facd7b0be1ef9ee6bb0e2e72
marshmallow0-0 <possibleWay23@gmail.com>
2024-10-06 14:48:10 +0900

    feat: 토큰 상태 추가

--- src/store/loginSlice.js
+++ src/store/loginSlice.js
@@ -3,6 +3,7 @@ import { createSlice } from '@reduxjs/toolkit';
 const initialLoginState = {
     isAuthenticated: false,
     user: null,
+    token: null,
 };
 
 const loginSlice = createSlice({
@@ -11,11 +12,16 @@ const loginSlice = createSlice({
     reducers: {
         login(state, action) {
             state.isAuthenticated = true;
-            state.user = action.payload;
+            // state.user = action.payload.user;  // 사용자 정보 저장
+            state.token = action.payload.token;  // 토큰 저장
+        },
+        setUser(state, action) {
+            state.user = action.payload;  // 사용자 정보 저장
         },
         logout(state) {
             state.isAuthenticated = false;
             state.user = null;
+            state.token = null;  // 로그아웃 시 토큰도 제거
         },
     },
 });
